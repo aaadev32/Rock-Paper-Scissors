526654aec841266985f14eb98966526b2115159a
aaadev32 <aaadev32@gmail.com>
2022-02-01 09:32:25 -0800

    added computerPlay function. nearly finished playRound function

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Rock-Paper-Scissors
+create a rock paper scissors game that will be played from the chrome dev tools console
+
+prompt user to input rock paper or scissors
+
+the input will be used in a function that pits the users choice against a computers choice.
+
+display the winner based off of their decisions 
---
+++ Rock-Paper-Scissors.js
@@ -0,0 +1,55 @@
+const player = window.prompt("Rock, Paper, or Scissors?");
+const playerSelection = player.toLowerCase();
+const computerSelection = computerPlay();
+
+function computerPlay() {
+    result = Math.floor(Math.random() * 3)
+    if (result = 0) {
+        selection = "rock"
+    } else if (result = 1) {
+        selection = "paper"
+    } else if (result = 2) {
+        selection = "scissors";
+    }
+
+    console.log(result);
+    console.log(selection);
+    return selection;
+
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        console.log("Paper Beats Rock!");
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        console.log("Rock Beats Scissors!");
+    } else if (playerSelection == "rock" && computerSelection == "rock") {
+        console.log("Its A Tie!");
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        console.log("Paper Beats Rock!");
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        console.log("Its A Tie!");
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        console.log("Scissors Beats Paper!");
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        console.log("Rock Beats Scissors!");
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        console.log("Scissors Beats Paper!");
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        console.log("Its A Tie!");
+    }
+    console.log(playRound(playerSelection, computerSelection));
+}
+
+
+
+if (playerSelection != "rock" || playerSelection != "paper" || playerSelection != "scissors") {
+    alert("error please refresh and use one of the prompted inputs.");
+} else {
+    playRound()
+}
+
+//need to figure how to call the function to test if the code even works
+
+
