395a6dc9d88d81e9d3aed3a38013303b8bd72086
aaadev32 <aaadev32@gmail.com>
2022-02-01 11:25:57 -0800

    playRound function progress

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,14 +1,15 @@
-const player = window.prompt("Rock, Paper, or Scissors?");
-const playerSelection = player.toLowerCase();
+const input = window.prompt("Rock, Paper, or Scissors?");
+const playerSelection = input.toLowerCase();
 const computerSelection = computerPlay();
 
 function computerPlay() {
-    result = Math.floor(Math.random() * 3)
-    if (result = 0) {
-        selection = "rock"
-    } else if (result = 1) {
-        selection = "paper"
-    } else if (result = 2) {
+    const result = Math.floor(Math.random() * 3);
+    let selection = "";
+    if (result == 0) {
+        selection = "rock";
+    } else if (result == 1) {
+        selection = "paper";
+    } else if (result == 2) {
         selection = "scissors";
     }
 
@@ -39,15 +40,15 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection == "scissors" && computerSelection == "scissors") {
         console.log("Its A Tie!");
     }
-    console.log(playRound(playerSelection, computerSelection));
+    //console.log(playRound(playerSelection, computerSelection)); maximum call stack exceeded
 }
 
 
 
-if (playerSelection != "rock" || playerSelection != "paper" || playerSelection != "scissors") {
-    alert("error please refresh and use one of the prompted inputs.");
-} else {
+if (playerSelection == "rock" || playerSelection == "paper" || playerSelection !== "scissors") {
     playRound()
+} else {
+    alert("error please refresh and use one of the prompted inputs.");
 }
 
 //need to figure how to call the function to test if the code even works
